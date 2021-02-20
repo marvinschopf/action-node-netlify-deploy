676b9f58205995dd15b10bc2525f777ab2ce33f0
Marvin <marvin@schopf.biz>
2021-02-20 17:48:24 +0100

    Use npx instead of yarn

--- action.sh
+++ action.sh
@@ -4,9 +4,7 @@
 export NETLIFY_SITE_ID=$NETLIFY_SITE_ID
 export NETLIFY_AUTH_TOKEN=$NETLIFY_AUTH_TOKEN
 
-yarn global add netlify-cli
-
-COMMAND="netlify deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
+COMMAND="npx netlify-cli deploy --dir=$BUILD_DIRECTORY --functions=$FUNCTIONS_DIRECTORY --message=\"$INPUT_NETLIFY_DEPLOY_MESSAGE\""
 
 if [[ $NETLIFY_DEPLOY_TO_PROD == "true" ]]
 then
